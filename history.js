f6d651bff5914b8cc637dd6e417bce27d952dbe5
vfa01-wi260046-web <vfa-wi260046@sankogakuen.jp>
2026-06-02 14:42:20 +0900

    4 個目のお宝みっけ

--- history.js
+++ history.js
@@ -4,5 +4,5 @@ const gameHistory = [
 { turn: 1, player: "playerA", finalPos: "川崎", event: "無事に移動した！", foundTreasure: false },
 { turn: 2, player: "playerA", finalPos: "厚木", event: "無事に移動した！", foundTreasure: false },
 { turn: 3, player: "playerA", finalPos: "大和", event: "🎉 第1の伝説のお宝をついに発見！！ 🎉", foundTreasure: true },
-
+{ turn: 4, player: "playerD", finalPos: "藤沢", event: "無事に移動した！", foundTreasure: false },
 ];
