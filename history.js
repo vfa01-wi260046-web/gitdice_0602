7015739f9233743ceb7dfadc2028e6b7248cc4d3
vfa01-wi260046-web <vfa-wi260046@sankogakuen.jp>
2026-06-02 14:00:41 +0900

    1個目のお宝みっけ

--- history.js
+++ history.js
@@ -1,7 +1,8 @@
 // ⚠️ 各ターンの終わりに、画面に表示された自分の結果コードをこの配列の中に追加してください。
 const gameHistory = [
     // ここから下に追記していく
-{ turn: 1, player: "playerA", finalPos: "秦野", event: "無事に移動した！", foundTreasure: false },
-{ turn: 1, player: "playerA", finalPos: "新百合丘", event: "💡 ヒント発見！ 村人：「南西へ向かい、町と町を繋ぐ大きな交差点となる場所だべ」", foundTreasure: false },
+{ turn: 1, player: "playerA", finalPos: "川崎", event: "無事に移動した！", foundTreasure: false },
+{ turn: 2, player: "playerA", finalPos: "厚木", event: "無事に移動した！", foundTreasure: false },
+{ turn: 3, player: "playerA", finalPos: "大和", event: "🎉 第1の伝説のお宝をついに発見！！ 🎉", foundTreasure: true },
 
 ];
